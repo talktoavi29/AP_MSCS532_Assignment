38f6e15cc34d6c827737dc830074a224b52405fa
talktoavi29 <37131576+talktoavi29@users.noreply.github.com>
2025-07-06 19:52:59 -0500

    dynamic

--- Assignment1.py
+++ Assignment1.py
@@ -1,11 +1,13 @@
-arr = [5, 2, 9, 1, 6]
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
-for i in range(1, len(arr)):
-    key = arr[i]
-    j = i - 1
-    while j >= 0 and arr[j] < key:
-        arr[j + 1] = arr[j]
-        j -= 1
-    arr[j + 1] = key
-
-print("Sorted in decreasing order:", arr)
+num_list = [10, 3, 7, 2, 8]
+sorted_list = insertion_sort(num_list)
+print("Sorted in decreasing order:", sorted_list)
